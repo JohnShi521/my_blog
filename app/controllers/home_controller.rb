93d5e48689d5663082d129d8bf3e51896977ea7c
virus <yystopf@163.com>
2018-09-07 17:10:31 +0800

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,28 @@
 class HomeController < ApplicationController
   layout("home_application")
+  before_action :pre_find
   def index
     @articles =Article.is_show_home
-    @new_articles = Article.is_show_home.order("created_at desc").limit(5)
-    @categories = Category.all
-    @time_file = Article.is_show_home.group_by{|o|o.create_time}
-    @tags = Tag.all
+    case params[:type]
+    when "technical"
+      @articles = @articles.where(type:"Articles::Technical")
+    when "life"
+      @articles = @articles.where(type:"Articles::Life")
+    else
+      @articles = @articles
+    end
+
   end
 
   def detail
     @article = Article.find_by_id(params[:id])
+  end
+
+  private
+  def pre_find
     @new_articles = Article.is_show_home.order("created_at desc").limit(5)
-    @categories = Category.includes(:articles).all
+    @categories = Category.all
     @time_file = Article.is_show_home.group_by{|o|o.create_time}
-    @tags = Tag.includes(:articles).all
+    @tags = Tag.all
   end
 end
