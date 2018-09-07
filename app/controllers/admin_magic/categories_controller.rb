96cdafc2fc9fb0dc9d8d717988a07ae0a1f8fcb5
virus <yystopf@163.com>
2018-09-07 11:21:08 +0800

    curd

--- app/controllers/admin_magic/categories_controller.rb
+++ app/controllers/admin_magic/categories_controller.rb
@@ -1,4 +1,64 @@
 class AdminMagic::CategoriesController < AdminMagic::BaseController
+  before_action :find_category, only:[:show,:edit,:update,:destroy]
+
   def index
+    @categories = Category.all
+
+    # 如果有分页
+    # @all = @all.page(params[:page]).per(page_count)
+  end
+
+
+  def new
+
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_magic_categories_path
+      flash[:success] = "新增成功"
+    else
+      render :new
+      flash[:error] = @category.errors.full_messages.join(",")
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    @category.attributes = category_params
+    if @category.save
+      redirect_to admin_magic_categories_path
+      flash[:success] = "更新成功"
+    else
+      render :edit
+      flash[:error] = @category.errors.full_messages.join(",")
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      redirect_to admin_magic_categories_path
+      flash[:success] = "删除成功"
+    else
+      flash[:error] = "删除失败"
+    end
+  end
+
+  private
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit!
   end
 end
--- app/controllers/admin_magic/tags_controller.rb
+++ app/controllers/admin_magic/tags_controller.rb
@@ -1,5 +1,65 @@
 class AdminMagic::TagsController < AdminMagic::BaseController
+  before_action :find_tag, only:[:show,:edit,:update,:destroy]
+
   def index
+    @tags = Tag.all
+
+    # 如果有分页
+    # @all = @all.page(params[:page]).per(page_count)
+  end
+
+
+  def new
 
   end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to admin_magic_tags_path
+      flash[:success] = "新增成功"
+    else
+      render :new
+      flash[:error] = @tag.errors.full_messages.join(",")
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    @tag.attributes = tag_params
+    if @tag.save
+      redirect_to admin_magic_tags_path
+      flash[:success] = "更新成功"
+    else
+      render :edit
+      flash[:error] = @tag.errors.full_messages.join(",")
+    end
+  end
+
+  def destroy
+    if @tag.destroy
+      redirect_to admin_magic_tags_path
+      flash[:success] = "删除成功"
+    else
+      flash[:error] = "删除失败"
+    end
+  end
+
+  private
+
+  def find_tag
+    @tag = Tag.find(params[:id])
+  end
+
+  def tag_params
+    params.require(:tag).permit!
+  end
+
 end
