f00990c2ab4c26edf9045e7fe8e51e02d97937e2
virus <yystopf@163.com>
2018-09-10 15:58:13 +0800

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,18 @@ class HomeController < ApplicationController
   layout("home_application")
   before_action :pre_find
   def index
-    @articles =Article.is_show_home.order("created_at desc")
+    @articles =Article.is_show_home
+    @articles =@articles.where(category_id:params[:category_id]) if params[:category_id].present?
+    @articles = @articles.joins(:tags).where("tags.id = ?", "#{params[:tag_id]}") if params[:tag_id].present?
+    @articles = @articles.where(create_time: params[:create_time].to_time) if params[:create_time].present?
+
     case params[:type]
     when "technical"
-      @articles = @articles.where(type:"Articles::Technical").page(params[:page]).per(10)
+      @articles = @articles.where(type:"Articles::Technical").order("created_at desc").page(params[:page]).per(10)
     when "life"
-      @articles = @articles.where(type:"Articles::Life").page(params[:page]).per(10)
+      @articles = @articles.where(type:"Articles::Life").order("created_at desc").page(params[:page]).per(10)
     else
-      @articles = @articles.page(params[:page]).per(10)
+      @articles = @articles.order("created_at desc").page(params[:page]).per(10)
     end
 
   end
